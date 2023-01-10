773751f84dfca26bd905999d848c5809e06dbaf1
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-01-10 22:59:35 +0000

    comments added

--- script.js
+++ script.js
@@ -89,6 +89,8 @@ let upperCasedCharacters = [
   'Y',
   'Z'
 ];
+
+
 let passwordLength = 0;
 let userChoice = [];
 let result = "";
@@ -105,14 +107,18 @@ let result = "";
 // }
 
 // Function to generate password with user input
+// while loop to only run if conditions are met
 function generatePassword() {
   while (passwordLength < 10 || passwordLength > 64 || passwordLength === "" || isNaN (passwordLength)){
     passwordLength = prompt ("How many characters would you like your password to be (please choose a number between 10 and 64 characters)?");
   }
+  // confirm messages to build and generate user choice characters
   const doYouSpecialCharacters = confirm("Would you like to add special characters?");
   const doYouNumericCharacters = confirm("Would you like to add numeric characters?");
   const doYouLowerCasedCharacters = confirm("Would you like to add lowercase characters?");
   const doYouUpperCasedCharacters = confirm("Would you like to add uppercase characters?");
+  
+  // if statements to add characters to user choice if they are true (selected as OK)
 if (doYouSpecialCharacters === true) {
   userChoice = userChoice.concat (specialCharacters)
 }
@@ -125,10 +131,12 @@ if (doYouLowerCasedCharacters === true) {
 if (doYouUpperCasedCharacters === true) {
   userChoice = userChoice.concat (upperCasedCharacters)
 }
+// if statement to show alert if all character confirms return as false (or cancel). URL will then refresh.
 if (doYouSpecialCharacters === false && doYouNumericCharacters === false && doYouLowerCasedCharacters === false && doYouUpperCasedCharacters === false) {
   alert("Please try again and ensure you select at least one character type")
   window.location.reload()
 }
+// for loop to generate random selection from the arrays and totalling to the result string
 console.log(userChoice);
 for(let i = 0; i < parseInt(passwordLength); i++) {
   result += userChoice [Math.floor(Math.random()*userChoice.length)]
